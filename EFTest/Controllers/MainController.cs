9391241cb5aca74db83e51de2e104a31085227fb
ll9 <llwierer@web.de>
2019-02-09 15:10:48 +0100

    Added Gridtabcontrol

---
+++ EFTest/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest.Controllers
+{
+    class MainController
+    {
+        private Form1 form1;
+
+        public MainController(Form1 form1)
+        {
+            this.form1 = form1;
+        }
+
+        internal void LoadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EFTest/Views/MainForm.cs
+++ EFTest/Views/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EFTest.Controllers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,14 @@ namespace EFTest
 {
     public partial class Form1 : Form
     {
+        private MainController _controller;
+
         public Form1()
         {
             InitializeComponent();
+
+            _controller = new MainController(this);
+            _controller.LoadData();
         }
     }
 }
