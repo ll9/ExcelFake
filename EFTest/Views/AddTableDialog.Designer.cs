eb7a73ad507a8415af510eee2555a8a76baf3a7b
ll9 <llwierer@web.de>
2019-02-11 19:34:12 +0100

    AddTable Dialog

--- EFTest/Views/AddTableDialog.Designer.cs
+++ EFTest/Views/AddTableDialog.Designer.cs
@@ -29,11 +29,124 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nameLabel;
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addTableViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.columnViewModelsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.columnViewModelsDataGridView = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            nameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.addTableViewModelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnViewModelsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnViewModelsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new System.Drawing.Point(38, 29);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new System.Drawing.Size(38, 13);
+            nameLabel.TabIndex = 1;
+            nameLabel.Text = "Name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.addTableViewModelBindingSource, "Name", true));
+            this.nameTextBox.Location = new System.Drawing.Point(82, 26);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(210, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // addTableViewModelBindingSource
+            // 
+            this.addTableViewModelBindingSource.DataSource = typeof(EFTest.ViewModels.AddTableViewModel);
+            // 
+            // columnViewModelsBindingSource
+            // 
+            this.columnViewModelsBindingSource.DataSource = typeof(EFTest.ViewModels.ColumnViewModel);
+            // 
+            // columnViewModelsDataGridView
+            // 
+            this.columnViewModelsDataGridView.AutoGenerateColumns = false;
+            this.columnViewModelsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.columnViewModelsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.dataTypeDataGridViewTextBoxColumn});
+            this.columnViewModelsDataGridView.DataBindings.Add(new System.Windows.Forms.Binding("DataSource", this.addTableViewModelBindingSource, "ColumnViewModels", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.columnViewModelsDataGridView.DataSource = this.columnViewModelsBindingSource;
+            this.columnViewModelsDataGridView.Location = new System.Drawing.Point(41, 61);
+            this.columnViewModelsDataGridView.Name = "columnViewModelsDataGridView";
+            this.columnViewModelsDataGridView.Size = new System.Drawing.Size(251, 220);
+            this.columnViewModelsDataGridView.TabIndex = 2;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // dataTypeDataGridViewTextBoxColumn
+            // 
+            this.dataTypeDataGridViewTextBoxColumn.DataPropertyName = "DataType";
+            this.dataTypeDataGridViewTextBoxColumn.HeaderText = "DataType";
+            this.dataTypeDataGridViewTextBoxColumn.Name = "dataTypeDataGridViewTextBoxColumn";
+            this.dataTypeDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataTypeDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // OKButton
+            // 
+            this.OKButton.DataBindings.Add(new System.Windows.Forms.Binding("Enabled", this.addTableViewModelBindingSource, "IsValid", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.OKButton.Location = new System.Drawing.Point(136, 297);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 3;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(217, 297);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Abbrechen";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // AddTableDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(303, 331);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.columnViewModelsDataGridView);
+            this.Controls.Add(nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "AddTableDialog";
             this.Text = "AddTableDialog";
+            ((System.ComponentModel.ISupportInitialize)(this.addTableViewModelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnViewModelsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnViewModelsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource addTableViewModelBindingSource;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.BindingSource columnViewModelsBindingSource;
+        private System.Windows.Forms.DataGridView columnViewModelsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn dataTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CancelButton;
     }
 }
--- EFTest/Views/AddTableDialog.cs
+++ EFTest/Views/AddTableDialog.cs
@@ -16,5 +16,17 @@ namespace EFTest.Views
         {
             InitializeComponent();
         }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
     }
 }
